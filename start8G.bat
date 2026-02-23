2b12709b2d4d5a73cae812725963ea7631b8566e
OverKot <sinkotro@gmail.com>
2026-02-23 12:30:40 +0200

    update start.bat

--- start8G.bat
+++ start8G.bat
@@ -1,6 +1,46 @@
 @echo off
-SET RAM=8G
-java -Xmx%RAM% -Xms%RAM% ^
+setlocal enabledelayedexpansion
+
+set RAM=8G
+set FORGE_JAR=forge-1.16.5-36.2.34.jar
+
+echo [INFO] Searching for Java 8...
+
+set "JAVA_EXEC="
+
+for /d %%i in (
+    "C:\Program Files\Eclipse Adoptium\jdk-8*"
+    "C:\Program Files\Eclipse Adoptium\jre-8*"
+    "C:\Program Files\Java\jre1.8*"
+    "C:\Program Files\Java\jdk1.8*"
+    "C:\Program Files\Zulu\zulu-8*"
+) do (
+    if exist "%%i\bin\java.exe" (
+        set "JAVA_EXEC=%%i\bin\java.exe"
+        goto :START_SERVER
+    )
+)
+
+java -version 2>&1 | findstr "1.8" >nul
+if %errorlevel% equ 0 (
+    set "JAVA_EXEC=java"
+    goto :START_SERVER
+)
+
+echo.
+echo [ERROR] Java 8 was NOT found!
+echo Found versions in Adoptium folder:
+dir /b "C:\Program Files\Eclipse Adoptium\"
+echo.
+echo Please install Java 8 (Temurin 8 LTS) or check paths.
+pause
+exit
+
+:START_SERVER
+echo [SUCCESS] Found Java 8 at: "%JAVA_EXEC%"
+echo.
+
+"%JAVA_EXEC%" -Xmx%RAM% -Xms%RAM% ^
 -Dfml.readTimeout=180 ^
 -Dfml.loginTimeout=180 ^
 -XX:+UseG1GC ^
@@ -21,6 +61,6 @@ java -Xmx%RAM% -Xms%RAM% ^
 -XX:SurvivorRatio=32 ^
 -XX:+PerfDisableSharedMem ^
 -XX:MaxTenuringThreshold=1 ^
--jar forge-1.16.5-36.2.34.jar nogui
+-jar %FORGE_JAR% nogui
 
 pause
